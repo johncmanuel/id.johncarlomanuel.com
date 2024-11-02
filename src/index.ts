5f8b087f15200ffd9bb2b99a4b433a7ea602bdd2
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2024-11-02 12:55:53 -0700

    remove hono

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,28 @@
 import { graph } from "./john";
-import { Hono } from "hono";
 
-const app = new Hono();
+export default {
+	async fetch(req: Request) {
+		const url = new URL(req.url);
 
-app.get("/", (c) => {
-	return new Response(JSON.stringify(graph), {
-		headers: {
-			"Content-Type": "application/json",
-			"Cache-Control": "no-cache",
-		},
-	});
-});
+		if (url.pathname !== "/") {
+			return new Response("Not found", {
+				status: 404,
+				headers: { "Content-Type": "text/plain" },
+			});
+		}
 
-export default app;
+		if (req.method !== "GET") {
+			return new Response("Method not allowed", {
+				status: 405,
+				headers: { "Content-Type": "text/plain" },
+			});
+		}
+
+		return new Response(JSON.stringify(graph), {
+			headers: {
+				"Content-Type": "application/json",
+				"Cache-Control": "no-cache",
+			},
+		});
+	},
+};
