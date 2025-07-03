6a9e886eaae2e8026b2677154bb40b2908e5f40b
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2025-07-03 10:08:04 -0700

    allow all

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
 import { graph } from "./john";
 
-// sites that're allowed to fetch the data
-const allowedOrigins = [
-	"https://johncarlomanuel.com/"
-]
-
 export default {
 	async fetch(req: Request) {
 		const url = new URL(req.url);
@@ -27,7 +22,7 @@ export default {
 			headers: {
 				"Content-Type": "application/json",
 				"Cache-Control": "no-cache",
-				"Access-Control-Allow-Origin": allowedOrigins.join(", "),
+				"Access-Control-Allow-Origin": "*",
 			},
 		});
 	},
